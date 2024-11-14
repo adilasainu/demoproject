2d804050bebfbd79625cec1a7b478539be907dee
adilasainu <adilasainudeen@gmail.com>
2024-11-14 23:27:11 +0530

    added2

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from cart.models import Cart,Order_details,Payments
 # Register your models here.
+from django.http import HttpResponse
+
 
 admin.site.register(Cart)
 admin.site.register(Order_details)
